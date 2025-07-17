40071033f92c91ea516b8a2f22093e2494e0a3d9
Saman Rashid <samanrashid@192.168.1.4>
2025-07-17 19:14:34 +0530

    counter using useState live coding problem 1

---
+++ practice2/src/Practice.js
@@ -0,0 +1,25 @@
+
+//Create a Counter Component
+import React, { useState } from 'react';
+import './Practice.css';
+
+const Practice = () => {
+  const [inc, showInc] = useState(0);
+  function handleIncrement(){
+    showInc(inc + 1);
+  }
+  function handleDecrement(){
+    showInc(inc -1);
+  }
+  return (
+    <>
+    <div className='show-text'>the Number is : {inc}</div>
+    <div className='Container'>
+    <button onClick={handleIncrement} className='inc-btn'>Click For Increment</button>
+    <button onClick={handleDecrement} className='dec-btn'>Click For Decrement</button>
+    </div>
+    </>
+  )
+}
+
+export default Practice
